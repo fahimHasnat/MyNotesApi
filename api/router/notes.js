24466bcb6f78ead215c2b17d153bf227756b91a3
fahimHasnat <fahimcsespring@gmail.com>
2019-06-16 15:48:28 +0600

    first commit

---
+++ api/router/notes.js
@@ -0,0 +1,113 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+const Note = require("../models/note");
+
+router.get("/", (req, res, next) => {
+  Note.find()
+    .select("title content date")
+    .exec()
+    .then(docs => {
+      const response = {
+        count: docs.length,
+        notes: docs.map(doc => {
+          return {
+            title: doc.title,
+            content: doc.content,
+            date: doc.date,
+            request: {
+              type: "GET",
+              url: "http://localhost:5000/notes/" + doc._id
+            }
+          };
+        })
+      };
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+router.post("/", (req, res, next) => {
+  const note = new Note({
+    _id: new mongoose.Types.ObjectId(),
+    title: req.body.title,
+    content: req.body.content,
+    date: req.body.date
+  });
+  note
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(200).json({
+        message: "Created Product Successfully",
+        createdNote: {
+          title: result.title,
+          content: result.content,
+          date: result.date,
+          request: {
+            type: "GET",
+            url: "http://localhost:5000/notes/" + result._id
+          }
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+router.get("/:NotesId", (req, res, next) => {
+  const id = req.params.NotesId;
+  Note.findById(id)
+    .select("title content date")
+    .exec()
+    .then(doc => {
+      console.log(doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res.status(404).json({
+          message: "No valid Entry Found"
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+router.delete("/:NotesId", (req, res, next) => {
+  const id = req.params.NotesId;
+
+  Note.remove({ _id: id })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: "Product Deleted",
+        request: {
+          type: "POST"
+        }
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+  res.status(200).json({
+    message: "Deleting SIngle GET requests"
+  });
+});
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const notesRoutes = require("./api/router/notes");
+
+mongoose.connect(
+  "mongodb+srv://fahimHasnat:" +
+    process.env.MONGO_ATLAS_PW +
+    "@cluster0-rici7.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
+
+mongoose.Promise = global.Promise;
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Headers",
+    "Origin , X-Requested-With, Content-Type, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.send(200).json({});
+  }
+  next();
+});
+
+app.use("/notes", notesRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+module.exports = app;
